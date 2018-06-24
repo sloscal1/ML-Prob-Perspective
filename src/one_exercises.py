c7dc410573febc9e70e6bce6dd1058ae6c8be874
Steve Loscalzo <sloscalzo85@gmail.com>
2018-06-24 10:34:46 -0400

    Question 3 is working, refactored kNN code.

--- src/one_exercises.py
+++ src/one_exercises.py
@@ -1,4 +1,6 @@
+import math
 import shutil
+import time
 from pathlib import Path
 from gzip import GzipFile
 
@@ -8,6 +10,7 @@ from scipy.stats import mode
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 from pyflann import *
 
@@ -57,6 +60,7 @@ def read_mnist_labels(local):
         df = pd.read_csv(local+".csv", sep=",")
     return df
 
+
 class LinearKNN(object):
     def __init__(self, k):
         if not (k % 2):
@@ -69,19 +73,58 @@ class LinearKNN(object):
         self.X = X
         self.y = y
 
+    def predict(self, X, batch_size=2048):
+        # Split up the computation because the matrix gets too
+        # big for memory if done at once.
+        # The appropriate batch size should be figured out per machine
+        # and data dimensionality.
+        preds = []
+        batches = int(math.ceil(X.shape[0]/batch_size))
+        for batch in range(batches):
+            start = batch*batch_size
+            end = min((batch+1)*batch_size, X.shape[0])
+
+            # Find the distance from every point to the training data
+            # This could be done using partial evaluation to speed this up.
+            dists = cdist(self.X, X[start:end])
+            # Each row is the distance from all test points to 1 training sample
+            # I need the min k values in each column
+            partitioned = np.argpartition(dists, self.k, axis=0)
+            nearest = np.squeeze(partitioned[:self.k, :])
+            # Get the labels corresponding to the min positions
+            labels = np.repeat(self.y.values, self.k, axis=0)
+            if self.k > 1:
+                next_preds = np.squeeze(mode(labels[nearest], axis=0).mode)
+            next_preds = np.squeeze(labels[nearest])
+            preds = preds + next_preds.tolist()
+        return preds
+
+class FLANN_KNN(object):
+    def __init__(self, k):
+        if not (k % 2):
+            raise ValueError("k must be odd to break ties")
+        self.k = k
+        self.X = None
+        self.y = None
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+
     def predict(self, X):
-        # Find the distance from every point to the training data
-        # This could be done using partial evaluation to speed this up.
-        dists = cdist(self.X, X)
-        # Each row is the distance from all test points to 1 training sample
-        # I need the min k values in each column
-        partitioned = np.argpartition(dists, self.k, axis=0)
-        nearest = np.squeeze(partitioned[:self.k, :])
+        flann = FLANN()
+        nearest, _ = flann.nn(
+                self.X.values,
+                X.values,
+                self.k,
+        )
+
         # Get the labels corresponding to the min positions
         labels = np.repeat(self.y.values, self.k, axis=0)
         if self.k > 1:
-            return np.squeeze(mode(labels[nearest], axis=0).mode)
+            return np.squeeze(mode(labels[nearest], axis=1).mode)
         return np.squeeze(labels[nearest])
+
 
 def question_1():
     if not (
@@ -120,15 +163,12 @@ def question_1():
     kNN = LinearKNN(1)
     kNN.fit(train_df.drop(columns=["target"]), train_df.target)
     preds = kNN.predict(test_df[:1000].drop(columns=["target"]))
-    error_rate = [accuracy_score(preds, test_df[:1000].target)]
-    print(f"Error rate of first 1000: {100-error_rate[-1]*100:0.2f}%")
+    error_rate = accuracy_score(preds, test_df[:1000].target)
+    print(f"Error rate of first 1000: {100-error_rate*100:0.2f}%")
     
-    # Couldn't do all 10000 on my machine in one go.
-    for end_val in range(2000, 10001, 1000):
-        preds = kNN.predict(test_df[end_val-1000:end_val].drop(columns=["target"]))
-        error_rate.append(accuracy_score(preds, test_df[end_val-1000:end_val].target))
-        print(f"Error rate of samples {end_val-1000} to {end_val}: {100-error_rate[-1]*100:0.2f}%")
-    print(f"Overall error rate: {100-np.mean(error_rate)*100:0.2f}%")
+    preds = kNN.predict(test_df.drop(columns=["target"]))
+    error_rate = accuracy_score(preds, test_df.target)
+    print(f"Overall error rate: {100-error_rate*100:0.2f}%")
 
     # Doing the shuffling part:
     np.random.seed = 1337
@@ -161,21 +201,60 @@ def question_2():
     python setup.py install
     ```
 
+    In the end, the speedup is quite drastic. As the sample size doubles
+    from 500 to 1000 to 2000 there is almost no change in speed for the FLANN
+    version; however, the time increases substantially for the Linear version.
+
     .. _yourself: https://github.com/primetang/pyflann/issues/1
     """
-
     dtypes = dict([(num, np.uint8) for num in range(28*28)])
     train_df = pd.read_csv("cached_data/mnist_train.csv", sep=",", dtype=dtypes)
     test_df = pd.read_csv("cached_data/mnist_test.csv", sep=",", dtype=dtypes)
 
-    flann = FLANN()
-    result, dists = flann.nn(
-            train_df.drop(columns=["target"]).values,
-            test_df.drop(columns=["target"]).values,
-            1
+    results = []
+    for samps in [500, 1000, 2000]:
+        for (alg, name) in [(FLANN_KNN(1), "flann"), (LinearKNN(1), "linear")]:
+            start = time.time()
+            alg.fit(train_df.drop(columns=["target"]), train_df.target)
+            preds = alg.predict(test_df[:samps].drop(columns=["target"]))
+            end = time.time()
+            error_rate = 100-accuracy_score(preds, test_df[:samps].target)*100
+            results.append([name, samps, end-start, error_rate])
+    results_df = pd.DataFrame(
+        results,
+        columns=[
+            'algorithm',
+            'n',
+            'time',
+            'error_rate'
+        ],
     )
-    print(result)
+    print(results_df)
+
+
+def question_3():
+    dtypes = dict([(num, np.uint8) for num in range(28*28)])
+    train_df = pd.read_csv("cached_data/mnist_train.csv", sep=",", dtype=dtypes)
+    test_df = pd.read_csv("cached_data/mnist_test.csv", sep=",", dtype=dtypes)
+
+    data = []
+    kNN = FLANN_KNN(1)
+    kNN.fit(train_df.drop(columns=["target"]), train_df.target)
+    for k in range(1, 11):
+        kNN.k = k
+        train_preds = kNN.predict(train_df.drop(columns=["target"]))
+        train_acc = accuracy_score(train_preds, train_df.target)
+        test_preds = kNN.predict(test_df.drop(columns=["target"]))
+        test_acc = accuracy_score(test_preds, test_df.target)
+        data.append([k, train_acc, test_acc])
+    rates = pd.DataFrame(data, columns=["k", "train_acc", "test_acc"])
+    
+    plt.title("kNN Accuracy Rates for Given k")
+    plt.plot("k", "train_acc", data=rates)
+    plt.plot("k", "test_acc", data=rates)
+    plt.show()
+        
 
 
 if __name__ == "__main__":
-    question_2()
+    question_3()
